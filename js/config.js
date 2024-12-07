c0d0841ca7b4becf6ecb0c531cf9319a2cb18b00
Sathwika Parshaboina <55009076+sathwika05@users.noreply.github.com>
2024-12-07 17:04:43 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_035U8QpL7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '34296mdapck275pja24ncodnvi, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '34296mdapck275pja24ncodnvi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
