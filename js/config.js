16af64368aa844bbf97f885c0f6340318bb27a26
Sathwika Parshaboina <55009076+sathwika05@users.noreply.github.com>
2024-12-07 15:15:47 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_035U8QpL7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '34296mdapck275pja24ncodnvi, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
